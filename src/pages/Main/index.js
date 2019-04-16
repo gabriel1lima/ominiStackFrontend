9f6e4ce51ad3b21228609b1b429e3161bd04473f
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-04-16 17:17:17 -0300

    Criando estrutura e estiliação da pagina inicial (Main)

---
+++ src/pages/Main/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../../assets/logo.png';
+import './styles.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div id="main-container">
+        <form>
+          <img src={logo} alt="" />
+          <input
+            placeholder="Criar um box"
+          />
+          <button type="submit">Criar</button>
+        </form>
+      </div>  
+    );
+  }
+}
